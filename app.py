6213c7417cbafbbdda78a3c85f28437d6e076c03
Sutanu Chatterjee <118735895+ronnismad@users.noreply.github.com>
2023-08-11 03:39:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,18 +14,22 @@ def get_base64(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
     
-def set_background(png_file):
-    bin_str = get_base64(png_file)
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file2(png_file) 
     page_bg_img = '''
     <style>
     .stApp {
     background-image: url("data:image/png;base64,%s");
-    background-size: cover;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-attachment: scroll; # doesn't work
     }
     </style>
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    set_background('./background.png')
+    return
+
+set_background('./background.png')
 
 def calculate_values(Air_Gap, No_of_Turns, Metal_Shield):
     if Air_Gap >= 5 and Air_Gap <= 510:
