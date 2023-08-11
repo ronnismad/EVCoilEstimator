212f0978c00b83e8d2d5ac3045bda92e260c21c6
Sutanu Chatterjee <118735895+ronnismad@users.noreply.github.com>
2023-08-11 05:00:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def main():
     
     st.markdown("<h2 style='font-size: 34px;'>EV Wireless Charging | Coil Parameter Estimator</h2>", unsafe_allow_html=True)
     st.markdown("<h2 style='font-size: 16px;'>developed by S.Chatterjee | School of Physics and Engineering | ITMO University </h2>", unsafe_allow_html=True)
-    st.markdown("<h2 style='font-size: 16px;color:green;'>Conforms to the SAE J2953_202010 Standard (https://www.sae.org/standards/content/j2954_202010/)E</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='font-size: 18px;color:lightgreen;'>Conforms to the SAE J2953_202010 Standard</h2>", unsafe_allow_html=True)
     st.write("This estimator utilises fitted curves of previously performed FEM simulation results and the application is programmed to solves quadratic and cubic polynomial functions in a real-time mode in order to estimate coil parameter values instantly, based on two major variables that are of much importance to WPT systems: Air Gap between the coils and Number of Turns of the individual coils.")
 
     st.markdown("<h2 style='font-size: 28px;'>PLEASE SELECT THE VALUES</h2>", unsafe_allow_html=True)
